e48613298f911372201b8c3bd0c3bb65d14bd68a
Tsai220 <tsai916mark@gmail.com>
2023-10-24 20:49:13 +0800

    大修改: 新增改def search，向證卷所尋找掛牌代碼。 原def search功能改名def analyze
    由新def search進入第一步驟

--- main.py
+++ main.py
@@ -6,48 +6,72 @@ from io import StringIO
 import model.eps as epsRate
 import model.chart as chart
 import time
-import  numpy as np
-stockTypeTxt="\n0.全部\n01.水泥工業\n02.食品工業\n03.塑膠工業\n04.紡織纖維\n05.電機機械\n06.電器電纜\n08.玻璃陶瓷\n09.造紙工業\n10.鋼鐵工業\n11.橡膠工業\n12.汽車工業\n13.電子工業\n14.建材營造業\n15.航運業\n16.觀光餐旅\n17.金融保險業\n18.貿易百貨業\n19.綜合\n20.其他業\n21.化學工業\n22.生技醫療業\n23.油電燃氣業\n24.半導體業\n25.電腦及週邊設備業\n26.光電業\n27.通信網路業\n28.電子零組件業\n29.電子通路業\n30.資訊服務業\n31.其他電子業\n32.文化創意業\n33.農業科技業\n34.電子商務\n35.綠能環保\n36.數位雲端\n37.運動休閒\n38.居家生活"
+
+stockTypeTxt = "\n0.全部\n01.水泥工業\n02.食品工業\n03.塑膠工業\n04.紡織纖維\n05.電機機械\n06.電器電纜\n08.玻璃陶瓷\n09.造紙工業\n10.鋼鐵工業\n11.橡膠工業\n12.汽車工業\n13.電子工業\n14.建材營造業\n15.航運業\n16.觀光餐旅\n17.金融保險業\n18.貿易百貨業\n19.綜合\n20.其他業\n21.化學工業\n22.生技醫療業\n23.油電燃氣業\n24.半導體業\n25.電腦及週邊設備業\n26.光電業\n27.通信網路業\n28.電子零組件業\n29.電子通路業\n30.資訊服務業\n31.其他電子業\n32.文化創意業\n33.農業科技業\n34.電子商務\n35.綠能環保\n36.數位雲端\n37.運動休閒\n38.居家生活"
 pandas.set_option('display.max_columns', None)
 pandas.set_option('display.max_rows', None)
 
 print('投資一定有風險，投資有賺有賠，該程式僅為分析之用，僅供參考')
 print(
-    '查詢股票市場別(請輸入類別)' + '\n1.上市股票' + '\n2.上櫃股票' + '\n3.上市和上櫃股票' + '\n4.輸入股票代碼' + '\n5.結束程式')
+    '-----\n查詢股票市場別(請輸入類別)' + '\n1.上市股票' + '\n2.上櫃股票' + '\n3.上市和上櫃股票' + '\n4.輸入股票代碼' + '\n5.結束程式')
 choose = str(input("請輸入類別: "))
 
 
-def search(stockNumber):
-    print(stockNumber)
-    for stock in stockNumber:
+def search(stockNumber, market, issueType, industry_code):
+    market = market
+    issueType = issueType
+    industry_code = industry_code
+    for index, stock in enumerate(stockNumber):
         try:
-            url = "https://goodinfo.tw/tw/StockFinDetail.asp?RPT_CAT=XX_M_QUAR_ACC&STOCK_ID=" + str(stock)
+            url = "https://isin.twse.com.tw/isin/class_main.jsp?owncode=" + stock + "&stockname=&isincode=&market=" + market + "&issuetype=" + issueType + "&industry_code=" + industry_code + "&Page=1&chklike=Y"
             request = req.Request(url, headers={
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'
             })
             with req.urlopen(request) as response:
-                data = response.read().decode('utf-8')
+                data = response.read().decode('MS950')
 
             root = bs4.BeautifulSoup(data, "lxml")
-            catchTable = StringIO(str(root.select_one('#tblFinDetail')))  # 先將html轉str在轉StringIO來變成可處理文本
-            if catchTable is not None:
-                dfs = pandas.read_html(catchTable)[0]
+            StockInfo = StringIO(str(root.select_one('.h4')))
 
-                epsRateOUT = epsRate.eps(dfs.loc[6, '2023Q2':'2021Q1'][::-1])
-                chart.chartAxis(epsRateOUT[0], epsRateOUT[1], stock)
-                print(epsRateOUT)
-                time.sleep(5)
+            if StockInfo is not None:
+                dfs = pandas.read_html(StockInfo)[0]
+
+                analyze(stockNumber=dfs.loc[1, 2], stockName=dfs.loc[1, 3])
+                time.sleep(1)
 
         except ValueError:
-            print(stock+" 未知股票代號")
+            print(stock + " 未知股票代號")
 
             continue
 
 
+def analyze(stockNumber, stockName):
+
+    try:
+        url = "https://goodinfo.tw/tw/StockFinDetail.asp?RPT_CAT=XX_M_QUAR_ACC&STOCK_ID=" + str(stockNumber)
+        request = req.Request(url, headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'
+        })
+        with req.urlopen(request) as response:
+            data = response.read().decode('utf-8')
+
+        root = bs4.BeautifulSoup(data, "lxml")
+
+        catchTable = StringIO(str(root.select_one('#tblFinDetail')))  # 先將html轉str在轉StringIO來變成可處理文本
+        if catchTable is not None:
+            dfs = pandas.read_html(catchTable)[0]
+
+            epsRateOUT = epsRate.eps(dfs.loc[6, '2023Q2':'2021Q1'][::-1])
+            chart.chartAxis(epsRateOUT[0], epsRateOUT[1], stockNumber, stockName)
+            print(stockNumber,epsRateOUT)
+            time.sleep(5)
+
+    except ValueError:
+        print(stockNumber + " 未知股票代號")
 
 
 if choose == '3':
-    print("------"+stockTypeTxt)
+    print("------" + stockTypeTxt)
     enter = str(input("輸入產業別: "))
 
 elif choose == '4':
@@ -55,7 +79,7 @@ elif choose == '4':
     while True:
         enter = str(input("輸入股票代碼(輸入完畢請按Enter): "))
         if enter == "n" or enter == "no" or enter == "":
-            search(stocks)
+            search(stockNumber=stocks, market='', issueType='', industry_code='')
             break
         elif enter is not None and enter != "n" and enter != "no":
             stocks.append(enter)
--- model/chart.py
+++ model/chart.py
@@ -2,12 +2,12 @@ import  matplotlib.pyplot as plt
 import numpy as np
 plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
 plt.rcParams['axes.unicode_minus'] = False
-def chartAxis(val,val2,stockNumber):
+def chartAxis(val,val2,stockNumber,stockName):
 
     plt.plot(val)
     plt.plot(val2*len(val))
     plt.plot(val,'ro')
     plt.ylabel("成長幅度%")
     plt.xlabel("舊->新")
-    plt.title(" 成長幅度%     股票代號 "+str(stockNumber))
+    plt.title(" 成長幅度%  "+stockName+"("+str(stockNumber)+")")
     plt.show()
